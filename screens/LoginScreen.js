a44ac0a7b1c6ceb6deccb5d95b64fbeb951ac89e
Md. Rishad Khan <rishadkhan190@gmail.com>
2021-06-04 16:21:53 +0600

    add login

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,41 +1,64 @@
 import { Button, Input, Image } from "react-native-elements";
-import React, { useState } from 'react'
-import { StyleSheet, Text, View, StatusBar} from 'react-native';
-import image from '../images/5098293.jpg'
+import React, { useState } from "react";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
+import image from "../images/5098293.jpg";
+import { KeyboardAvoidingView } from "react-native";
 
-const LoginScreen = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    return (
-        <View>
-            <StatusBar style='light'/>
-            <Image source={{
-                uri: "https://ibb.co/PCdGbKr"
-            }}
-            style={{width: 200, height:200}}
-            />
-            <View style={styles.inputContainer}>
-                <Input 
-                placeholder="Email" 
-                autoFocus
-                type="email"
-                value={email}
-                onChangeText={(text)=> setEmail(text)}
-                />
-                <Input 
-                placeholder="Password" 
-                secureTextEntry 
-                type="password"
-                value={password}
-                onChangeText={(text)=> setPassword(text)}
-                />
-            </View>
-        </View>
-    )
-}
+const LoginScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const signIn = () => {};
+  return (
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+      <StatusBar style="light" />
+      <Image
+        source={{
+          uri: "https://ibb.co/PCdGbKr",
+        }}
+        style={{ width: 200, height: 200 }}
+      />
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Email"
+          autoFocus
+          type="email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <Input
+          placeholder="Password"
+          secureTextEntry
+          type="password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+      </View>
+      <Button containerStyle={styles.button} onPress={signIn} title="Login" />
+      <Button
+        containerStyle={styles.button}
+        type="outline"
+        onPress={() => navigation.navigate("Register")}
+        title="Register"
+      />
+    </KeyboardAvoidingView>
+  );
+};
 
-export default LoginScreen
+export default LoginScreen;
 
 const styles = StyleSheet.create({
-    inputContainer: {},
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 10,
+    backgroundColor: "white",
+  },
+  inputContainer: {
+    width: 200,
+  },
+  button: {
+    width: 100,
+    marginTop: 10,
+  },
+});
--- screens/RegScreen.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const RegScreen = () => {
-  return <div></div>;
-};
-
-export default RegScreen;
