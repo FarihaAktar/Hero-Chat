590b2af210df7b63a439cf6cf571397b0504bf65
mahamudul9 <mahmudulrana9@gmail.com>
2021-06-04 20:38:56 +0600

    1st

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -2,15 +2,17 @@ import { Button, Input, Image } from "react-native-elements";
 import React, { useState } from 'react'
 import { StyleSheet, Text, View, StatusBar} from 'react-native';
 import image from '../images/5098293.jpg'
+import { KeyboardAvoidingView } from "react-native";
 
 const LoginScreen = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const signIn={}
     return (
-        <View>
+        <KeyboardAvoidingView behavior='padding' style={styles.container}> 
             <StatusBar style='light'/>
             <Image source={{
-                uri: "https://ibb.co/PCdGbKr"
+                uri: "https://i.ibb.co/hKb8XVq/5098293.jpg"
             }}
             style={{width: 200, height:200}}
             />
@@ -30,12 +32,21 @@ const LoginScreen = () => {
                 onChangeText={(text)=> setPassword(text)}
                 />
             </View>
-        </View>
+            <Button containerStyle={styles.button} onPress={signIn} title="Login"/>
+            <Button containerStyle={styles.button} type="outline" title="Register"/>
+            <View style={{height:100}} />
+        </KeyboardAvoidingView>
     )
 }
 
 export default LoginScreen
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+    },
     inputContainer: {},
+    button: {}
 })
